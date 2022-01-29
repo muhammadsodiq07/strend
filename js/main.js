74b1865f3e0762a744ee13b5518c23e6d6e401b5
akbarinfo <girtbezbet@gmail.com>
2022-01-29 19:52:42 +0500

    index header hero full version

--- js/main.js
+++ js/main.js
@@ -10,3 +10,30 @@ searchbtn.addEventListener('click', function() {
 xbtn.addEventListener('click', function() {
   searchbox.classList.remove('search-open');
 });
+
+window.onscroll = () => {scrollNavbar()};
+
+scrollNavbar = () => {
+    // Target elements
+    const navBar = document.getElementById("id-header");
+    const links = document.querySelectorAll("#navBar a");
+
+  if (document.documentElement.scrollTop > 100) {
+    navBar.classList.add("pa-fixed-header");
+
+    // Change the color of links on scroll
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        element.classList.add('text-black');
+    }
+
+  } else {
+    navBar.classList.remove("pa-fixed-header");
+
+    // Change the color of links back to default
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        element.classList.remove('text-black');
+    }
+  }
+}
